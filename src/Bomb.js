d009820657c2b5a5db18a7a5cfaf0ea1c5a1855e
nachoespino53 <nachoespino53@gmail.com>
2018-11-19 14:04:27 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            secondsLeft: this.props.initialCount
+        }
+
+        
+    }
+
+    messageToDisplay() {
+        if (this.state.secondsLeft === 0) {
+            return "Boom!"
+        } else  {
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.messageToDisplay()}
+            </React.Fragment>
+        )
+    }
+}
